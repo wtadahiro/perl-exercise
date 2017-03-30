dca47bc805ed6f4e44d0ee9acbae2a4e82e6b651
tadahiro.wada <tadahiro.wada@d8aspring.com>
2017-03-30 19:35:16 +0900

    basic syntax

---
+++ lib/basic_syntaxes.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+
+# variable types
+## scalar
+my $string = 'foo';
+my $integer = 123;
+
+
+## array
+my @strings = ('dog', 'cat', 'owl');
+my @numbers = (1, 2, 3);
+my @mixed = ('dog', 1);
+
+### get a element by index
+# @strings[0] also work, but isn't recommended
+my $first = $strings[0]; # dog
+
+### the size of array, accessing an array in a scalar context
+my $size = @strings; # 3
+
+
+## Hash
+my %fruit_color = ('apple', 'red', 'banana', 'yellow');
+my %fruit_color_arrow_operator = (
+  apple => 'red',
+  banana => 'yellow',
+);
+
+### get a element of hash by key
+my $apple_color = $fruit_color{apple}; # red
+print $apple_color;
