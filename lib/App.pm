639d9a022733f9c0e3a98cff3308aa09576ee2c3
tadahiro.wada <tadahiro.wada@d8aspring.com>
2017-04-12 20:54:38 +0900

    tidy

--- lib/App.pm
+++ lib/App.pm
@@ -4,11 +4,11 @@ use warnings;
 use parent qw(Mojolicious);
 
 sub startup {
-  my $self = shift;
-  my $routes = $self->routes;
+    my $self   = shift;
+    my $routes = $self->routes;
 
-  $routes->namespaces(['App::Controller']);
-  $routes->get('/')->to('root#respond_hello_world');
+    $routes->namespaces(['App::Controller']);
+    $routes->get('/')->to('root#respond_hello_world');
 }
 
 1;
